9fa5a4f35f14fb65cbcec46b57bcce1b70954442
= <adamjcsmith@gmail.com>
2016-02-25 16:39:13 +0000

    Added huge JSON data set for diagnostics

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ var http = require('http');
 var url = require('url');
 var querystring = require('querystring');
 
-var jsonData = require('./data/offlinify-data.json');
 var _ = require('lodash');
+
+var jsonData = require('./data/offlinify-data.json');
+var jsonDataBig = require('./data/offlinify-data-big.json');
 
 var server = http.createServer(function(req, res) {
   var pathURL = url.parse(req.url).pathname;
@@ -13,16 +15,16 @@ var server = http.createServer(function(req, res) {
     res.writeHead(200, {"Content-Type": "text/html"});
     res.write("<!doctype html><html><body><p>This is the Offlinify API. Use /get and /post.</p></body></html>");
   }
-  else if(pathURL == '/get') {
+  else if(pathURL == '/get' || pathURL == '/getBig') {
     var params = querystring.parse(url.parse(req.url).query);
     res.writeHead(200, {"Content-Type": "application/JSON"});
     var filteredData;
+    if(pathURL == '/get') filteredData = jsonData;
+    else filteredData = jsonDataBig;
     if(params.after) {
-      filteredData = _.filter(jsonData, function(o) {
+      filteredData = _.filter(filteredData, function(o) {
         return Date.parse(o.timestamp) > Date.parse(params.after);
       });
-    } else {
-      filteredData = jsonData;
     }
     res.write(JSON.stringify(filteredData));
   }
