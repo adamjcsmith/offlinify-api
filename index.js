3bea508adbd20254a189435b1ae7f75a59ad46fe
= <adamjcsmith@gmail.com>
2016-03-04 15:01:48 +0000

    Fixed trailing slash

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ var server = http.createServer(function(req, res) {
     }
     res.write(JSON.stringify(filteredData));
   }
-  else if(pathURL == 'getBGMS') {
+  else if(pathURL == '/getBGMS') {
     var params = querystring.parse(url.parse(req.url).query);
     res.writeHead(200, {"Content-Type": "application/JSON"});
     var filteredData = jsonDataBGMS;
